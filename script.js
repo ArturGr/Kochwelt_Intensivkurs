2446ca80d651c30fd4a64b945270b1d146086ad2
volkat0r <volkerderksen@gmail.com>
2025-10-16 19:28:03 +0200

    random fixes

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function menuCalc(){
     // get number of portions to calculate with
     const portions = document.getElementById("portions").value;
     // get all values within the table, parse them into numbers/integer (if required) and store them within an array
-    const amount = Array.from(document.querySelectorAll(".amount")).map(el => parseInt(el.textContent));
+    const amount = Array.from(document.querySelectorAll(".amount span")).map(el => parseInt(el.textContent));
 
     console.log(portions, amount);
 };
