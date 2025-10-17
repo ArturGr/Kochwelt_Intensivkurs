e89a06665031c77eba2e5a641dda2a5126c19228
volkat0r <volkerderksen@gmail.com>
2025-10-17 14:36:50 +0200

    removing other script

--- script.js
+++ script.js
@@ -11,20 +11,8 @@ function toggleMenu(){
 }
 
 
-function menuCalc(){
-    // get number of portions to calculate with
-    const portions = document.getElementById("portions").value;
-    // get all values within the table, parse them into numbers/integer (if required) and store them within an array
-    const amount = Array.from(document.querySelectorAll(".amount span")).map(el => (textContent));
-
-    console.log(portions, amount);
-};
-
-
-
 /* Call different functions after DOM was loaded */
 document.addEventListener("DOMContentLoaded", () => {
     // toggle responsive Menu
     toggleMenu();
-    menuCalc();
 });
